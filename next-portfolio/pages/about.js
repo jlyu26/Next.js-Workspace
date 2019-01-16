9860df8e2c05e197ff9c1584008ecec144377481
jlyu-personal <lyujinyan@gmail.com>
2019-01-15 22:26:50 -0500

    Add a basic shared layout component.

--- next-portfolio/pages/about.js
+++ next-portfolio/pages/about.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default () => (
-	<div>
-		<h1>About</h1>
+	<Layout title="About">
 		<Link href="/">
 			<a>Home</a>
 		</Link>
 		<p>Full-Stack Developer</p>
 		<img src="/static/JavaScript-logo.png" alt="JavaScipt" height="200px" />		
-	</div>
+	</Layout>
 );
---
+++ next-portfolio/pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const HireMe = () => (
+	<Layout title="Hire Me">
+		<p>
+			You can reach to me at{" "}
+			<a href="mailto:example@server.com">example@server.com</a>
+		</p>
+	</Layout>
+);
+
+export default HireMe;
